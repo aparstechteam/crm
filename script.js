276dd43a813d12e3761bcecd75b0d9256a4702ad
Nasimul Hasan Deep <45994595+nasimulhasan513@users.noreply.github.com>
2021-02-24 10:21:58 +0600

    ui updated

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-// let count = document.getElementById("count");
-// let sales = document.getElementById("sales");
-// let earnings = document.getElementById("earnings");
+
 let dashboard = document.getElementById("dashboard");
 let links = document.getElementById("links");
 let terms = document.getElementById("terms");
+let controlpanelm = document.getElementById("controlpanelm");
+let upcoming_campaignm = document.getElementById("upcoming_campaignm");
+let giftsm = document.getElementById("giftsm");
+let donationm = document.getElementById("donationm");
 
-controlpanel = document.getElementById("controlpanel");
-upcoming_campaign = document.getElementById("upcoming_campaign");
-gifts = document.getElementById("gifts");
-donation = document.getElementById("donation");
+let controlpanel = document.getElementById("controlpanel");
+let upcoming_campaign = document.getElementById("upcoming_campaign");
+let gifts = document.getElementById("gifts");
+let donation = document.getElementById("donation");
 
 fetch(
   "https://script.google.com/macros/s/AKfycbwer7YqkBVBVV4gfeQUKvqwSVDyVNLjIe1oRQXEY2sKEN3LfpYZcvotxw/exec"
 )
   .then((data) => data.json())
   .then((res) => {
-    let { total_number, total_amount, total_earning } = res.data;
-    // count.innerHTML = total_number;
-    // sales.innerHTML = total_amount + " <span style='font-size:35px'>tk</span>";
-    // earnings.innerHTML =
-    //   total_earning + " <span style='font-size:35px'>tk</span>";
-    controlpanel.innerHTML = total_earning * 0.3;
-    upcoming_campaign.innerHTML = total_earning * 0.3;
-    gifts.innerHTML = total_earning * 0.3;
-    donation.innerHTML = total_earning * 0.1;
+    let { total_earning } = res.data;
+
+    controlpanelm.innerHTML =
+      total_earning * 0.3 ;
+    upcoming_campaignm.innerHTML =
+      total_earning * 0.3 ;
+    giftsm.innerHTML =
+      total_earning * 0.3;
+    donationm.innerHTML =
+      total_earning * 0.1 ;
+    controlpanel.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
+    upcoming_campaign.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
+    gifts.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
+    donation.innerHTML = total_earning * 0.1+ " <span style='font-size:35px'>tk</span>";
   });
-fetch(
-  "https://script.google.com/macros/s/AKfycbwer7YqkBVBVV4gfeQUKvqwSVDyVNLjIe1oRQXEY2sKEN3LfpYZcvotxw/exec"
-)
-  .then((data) => data.json())
-  .then((res) => {
-    let { total_number, total_amount, total_earning } = res.data;
-    count.innerHTML = total_number;
-    sales.innerHTML = total_amount + " <span style='font-size:35px'>tk</span>";
-    earnings.innerHTML =
-      total_earning + " <span style='font-size:35px'>tk</span>";
-  });
+
 setInterval(() => {
-  fetch(
-    "https://script.google.com/macros/s/AKfycbwer7YqkBVBVV4gfeQUKvqwSVDyVNLjIe1oRQXEY2sKEN3LfpYZcvotxw/exec"
-  )
-    .then((data) => data.json())
-    .then((res) => {
-      let { total_number, total_amount, total_earning } = res.data;
-      count.innerHTML = total_number;
-      sales.innerHTML =
-        total_amount + " <span style='font-size:35px'>tk</span>";
-      earnings.innerHTML =
-        total_earning + " <span style='font-size:35px'>tk</span>";
-    });
+ 
+fetch(
+  "https://script.google.com/macros/s/AKfycbwer7YqkBVBVV4gfeQUKvqwSVDyVNLjIe1oRQXEY2sKEN3LfpYZcvotxw/exec"
+)
+  .then((data) => data.json())
+  .then((res) => {
+    let {  total_earning } = res.data;
+  
+    controlpanel.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
+    upcoming_campaign.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
+    gifts.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
+    donation.innerHTML = total_earning * 0.1+ " <span style='font-size:35px'>tk</span>";
+  });
 }, 60000);
 
 let path = window.location.pathname;
