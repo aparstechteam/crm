539c02cb519340c93e75dc270d9e3fcde8637cec
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2021-02-28 19:09:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,18 +19,22 @@ fetch(
   .then((res) => {
     let { total_earning } = res.data;
 
-    controlpanelm.innerHTML =
-      total_earning * 0.3 ;
-    upcoming_campaignm.innerHTML =
-      total_earning * 0.3 ;
-    giftsm.innerHTML =
-      total_earning * 0.3;
-    donationm.innerHTML =
-      total_earning * 0.1 ;
-    controlpanel.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
-    upcoming_campaign.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
-    gifts.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
-    donation.innerHTML = total_earning * 0.1+ " <span style='font-size:35px'>tk</span>";
+     controlpanelm.innerHTML = Math.round(total_earning * 0.3);
+    upcoming_campaignm.innerHTML = Math.round(total_earning * 0.3);
+    giftsm.innerHTML = Math.round(total_earning * 0.3);
+    donationm.innerHTML = Math.round(total_earning * 0.1);
+    controlpanel.innerHTML =
+      Math.round(total_earning * 0.3) +
+      " <span style='font-size:35px'>tk</span>";
+    upcoming_campaign.innerHTML =
+      Math.round(total_earning * 0.3) +
+      " <span style='font-size:35px'>tk</span>";
+    gifts.innerHTML =
+      Math.round(total_earning * 0.3) +
+      " <span style='font-size:35px'>tk</span>";
+    donation.innerHTML =
+      Math.round(total_earning * 0.1) +
+      " <span style='font-size:35px'>tk</span>";
   });
 
 setInterval(() => {
@@ -42,10 +46,22 @@ fetch(
   .then((res) => {
     let {  total_earning } = res.data;
   
-    controlpanel.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
-    upcoming_campaign.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
-    gifts.innerHTML = total_earning * 0.3+ " <span style='font-size:35px'>tk</span>";
-    donation.innerHTML = total_earning * 0.1+ " <span style='font-size:35px'>tk</span>";
+    controlpanelm.innerHTML = Math.round(total_earning * 0.3);
+    upcoming_campaignm.innerHTML = Math.round(total_earning * 0.3);
+    giftsm.innerHTML = Math.round(total_earning * 0.3);
+    donationm.innerHTML = Math.round(total_earning * 0.1);
+    controlpanel.innerHTML =
+      Math.round(total_earning * 0.3) +
+      " <span style='font-size:35px'>tk</span>";
+    upcoming_campaign.innerHTML =
+      Math.round(total_earning * 0.3) +
+      " <span style='font-size:35px'>tk</span>";
+    gifts.innerHTML =
+      Math.round(total_earning * 0.3) +
+      " <span style='font-size:35px'>tk</span>";
+    donation.innerHTML =
+      Math.round(total_earning * 0.1) +
+      " <span style='font-size:35px'>tk</span>";
   });
 }, 60000);
 
